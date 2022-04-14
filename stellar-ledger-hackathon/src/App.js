b55656ac64298b7bc4a7ca3ee3167dae754f123e
GuilaneDen <83951892+GuilaneDen@users.noreply.github.com>
2022-04-14 18:51:13 +0200

    Init project

---
+++ stellar-ledger-hackathon/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Layout from './component/layout/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Layout/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
