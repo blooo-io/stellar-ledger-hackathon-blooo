c2b797380c5f793cb0ba91d53c38197e7f9db077
n4l5u0r <59141606+n4l5u0r@users.noreply.github.com>
2022-04-14 20:36:10 +0200

    fix: switch fix

--- src/component/layout/Layout.js
+++ src/component/layout/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Buyer from '../page/Buyer';
 import Landing from '../page/Landing';
 import Seller from '../page/Seller';
@@ -7,11 +7,11 @@ import Seller from '../page/Seller';
 const Layout = () => {
   return (
     <Router fallback={<span />}>
-      <Switch>
-        <Route path="/" exact component={Landing} />
-        <Route path="/seller" component={Seller}/>
-        <Route path="/buyer" component={Buyer} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Landing/>} />
+        <Route path="/seller" element={<Seller/>}/>
+        <Route path="/buyer" element={<Buyer/>} />
+      </Routes>
     </Router>
   )
 }
