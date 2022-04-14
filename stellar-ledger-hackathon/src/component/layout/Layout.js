4d1dc5ce2240cfa818016e3796e3229d61a51cb2
GuilaneDen <83951892+GuilaneDen@users.noreply.github.com>
2022-04-14 18:57:24 +0200

    feat(router): Added a router to the project

--- stellar-ledger-hackathon/src/component/layout/Layout.js
+++ stellar-ledger-hackathon/src/component/layout/Layout.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Buyer from '../page/Buyer';
+import Landing from '../page/Landing';
+import Seller from '../page/Seller';
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <Router fallback={<span />}>
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/seller" component={Seller}/>
+        <Route path="/buyer" component={Buyer} />
+      </Switch>
+    </Router>
   )
 }
 
